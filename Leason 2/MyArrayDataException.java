2096b45a724b353d88f38fe03a60f21dbf9e1720
Darko <nigmatulln0365@gmail.com>
2021-07-14 12:16:37 +0300

    Create Homework 2

---
+++ Leason 2/MyArrayDataException.java
@@ -0,0 +1,9 @@
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
